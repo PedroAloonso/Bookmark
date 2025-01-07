b0172d24d529ecbacad3f28bf10c9296b855c718
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-01-07 19:25:27 -0300

    chore: remove dotenv dependency and related configurations

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from "vite";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-const env = dotenv.config().parsed;
 
 export default defineConfig({
     plugins: [],
-    define: {
-        "process.env": env,
-    },
 });
