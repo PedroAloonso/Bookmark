d28ac41d69ec83f07d16ab7b3b8d6e802ddec078
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-01-07 19:25:17 -0300

    feat: add Google sign-in and sign-out functionality in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,37 @@
-export default function sidebarActions () {
-    const openSideBar = document.querySelector("#openSideBarBtn");
+import { signInWithGoogle, signOutGoogle, getUser } from "../firebase/auth.js";
+
+
+export default function sidebarActions() {
+
+    const openSidebar = document.querySelector("#openSideBarBtn");
     const closeSideBar = document.querySelector("#closeSideBarBtn");
     const sidebar = document.querySelector("#sidebar");
+    const signIn = document.querySelector("#userPhoto");
+    const signOut = document.querySelector("#signOutBtn");
 
-    openSideBar.addEventListener("click", () => {
+    // Ação para abrir e fechar a sidebar
+    openSidebar.addEventListener("click", () => {
         sidebar.classList.toggle("open");
-        openSideBar.classList.toggle("disable-button");
+        openSidebar.classList.toggle("disable-button");
     });
 
     closeSideBar.addEventListener("click", () => {
         sidebar.classList.toggle("open");
-        openSideBar.classList.toggle("disable-button");
+        openSidebar.classList.toggle("disable-button");
+    });
+    
+    // Ação para fazer login e logout
+    signIn.addEventListener("click", async () => {
+        await signInWithGoogle();
+        getUser().then((user) => {
+            signIn.setAttribute("src", user.photoURL);
+            signOut.classList.toggle("disable");
+        });
+    });
+
+    signOut.addEventListener("click", () => {
+        signOutGoogle();
+        signIn.setAttribute("src","img/user-icon.png");
+        signOut.classList.toggle("disable")
     });
 }
